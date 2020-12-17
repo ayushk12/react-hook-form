d50e5ee9d1ff1d30ae7628fcf94b93b62d926f78
Ayush Kumar <kayush242@gmail.com>
2020-12-17 12:20:42 +0530

    added style

--- src/App.js
+++ src/App.js
@@ -4,33 +4,39 @@ import useForm from "react-hook-form";
 
 function App() {
   const onSubmit = () => {};
+  const { register } = useForm();
   return (
     <form className="App" onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <label>First Name:</label>
-      <input name="firstName" />
+      <input name="firstName" placeholder="lastname" ref={register} />
 
       <label>Last Name:</label>
-      <input name="firstName" />
+      <input name="firstName" placeholder="lastname" ref={register} />
 
       <label>Gender</label>
-      <select name="gender">
+      <select name="gender" ref={register}>
         <option>Select...</option>
         <option>Male</option>
         <option>Female</option>
       </select>
 
       <label>Username</label>
-      <input name="username" />
+      <input name="username" placeholder="username" ref={register} />
 
       <label>Email</label>
-      <input name="email" />
+      <input name="email" placeholder="email" ref={register} />
       <label>Phone No</label>
+      <input name="phoneNo" placeholder="number" ref={register}></input>
 
       <label>About you</label>
-      <textarea name="about you" />
+      <textarea
+        name="about you"
+        placeholder="tell me about yourself"
+        ref={register}
+      />
 
-      <input type="submit" />
+      <input type="submit" ref={register} />
     </form>
   );
 }
