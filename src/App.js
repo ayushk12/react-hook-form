d9512047e2665f1891db17ef12c94d36b99b4c23
Ayush Kumar <kayush242@gmail.com>
2020-12-17 12:06:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./styles.css";
+import useForm from "react-hook-form";
+
+function App() {
+  const onSubmit = () => {};
+  return (
+    <form className="App" onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <label>First Name:</label>
+      <input name="firstName" />
+
+      <label>Last Name:</label>
+      <input name="firstName" />
+
+      <label>Gender</label>
+      <select name="gender">
+        <option>Select...</option>
+        <option>Male</option>
+        <option>Female</option>
+      </select>
+
+      <label>Username</label>
+      <input name="username" />
+
+      <label>Email</label>
+      <input name="email" />
+
+      <label>About you</label>
+      <textarea name="about you" />
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default App;
